423ad6167f76d3cc931bbbe4fe6cef67b56be010
Claire Schmit <cschm155@smail.uni-koeln.de>
2025-10-30 12:23:50 +0100

    Aufgabe 2 Zahlen und Wahrheitswerte

---
+++ src/p2/P2_main.java
@@ -0,0 +1,18 @@
+package p2;
+
+public class P2_main {
+
+	public static void main(String[] args) {
+		int i = 18;
+		boolean b = false;
+		
+		if (i > 10) 
+			b = true; 
+	System.out.println("i = " + i);
+	System.out.print("b = " + b);
+
+		
+			
+	}
+
+}
