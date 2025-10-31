d540cdaecf2f1128269fc07783c8f19c8c771e99
Claire Schmit <cschm155@smail.uni-koeln.de>
2025-10-31 20:53:14 +0100

    Hausaufgabe 3

---
+++ src/h3/h3_main.java
@@ -0,0 +1,36 @@
+package h3;
+
+public class h3_main {
+
+	public static void main(String[] args) {
+		int i = 200;
+		int j = 110;
+		int k = -10;
+		
+		if (i > j) {
+			if (i > 200)
+				if (j > 100) {
+					k = 3;
+				} else {
+					k = 2;
+				}
+		}  else {
+			if (!(j > 100)) {
+				k = 1;
+			} else 
+				k = -10;
+			
+		}   if (!(i > j) && !(i > 200) && !(j > 100))  {
+		 k = 4;
+	} else {
+		k = -10;
+		
+	}
+	System.out.println("i =" + i + "j =" + j + "k =" + k);
+		}
+	}
+
+
+
+
+
